9e82f643ebe803391488720bca175c620361aff8
InioluwaAdeyiga <92749690+InioluwaAdeyiga@users.noreply.github.com>
2024-07-06 16:04:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def generate_caption():
     return jsonify({"caption": caption})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 1000)))
